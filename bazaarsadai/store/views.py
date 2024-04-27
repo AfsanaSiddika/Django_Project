4496e34face2ff6da13d916412faa994bd268e82
AfsanaSiddika <afsanaesha584@gmail.com>
2024-04-27 15:04:57 +0600

    django final project

---
+++ bazaarsadai/store/views.py
@@ -0,0 +1,46 @@
+
+# Create your views here.
+from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
+import json
+
+def store(request):
+	products = Product.objects.all()
+	context = {'products': products}
+	return render(request, 'store/store.html', context)
+
+
+def cart(request):
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else :
+		items = []
+
+	context = {'items': items, 'order': order}
+	return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		items = []
+
+	context = {'items': items, 'order': order}
+	return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+	data = json.loads(request.data)
+	productId = data['productId']
+	action = data['action']
+
+	print('Action:', action)
+	print('productId:', productId)
+
+	return JsonResponse('Item was added', safe=False)
