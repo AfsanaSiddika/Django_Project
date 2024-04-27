e92e15a16c07fd148895ac74e44e39dad83ef27a
AfsanaSiddika <afsanaesha584@gmail.com>
2024-04-27 22:12:30 +0600

    updated views.py def store(request)

--- bazaarsadai/store/models.py
+++ bazaarsadai/store/models.py
@@ -14,7 +14,7 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-  #  digital = models.BooleanField(default=False, null=True, blank=False) #
+    digital = models.BooleanField(default=False, null=True, blank=False) #
     image = models.ImageField(upload_to='static/images/',null=True, blank=True)
 
     def __str__(self):
@@ -37,6 +37,15 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
 
     @property
     def get_cart_total(self):
