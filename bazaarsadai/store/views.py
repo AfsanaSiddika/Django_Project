c2d556d84ed6430c6d2801b6cdef9b8e69c68afb
AfsanaSiddika <afsanaesha584@gmail.com>
2024-04-29 18:30:32 +0600

    add new fuctions login signup

--- bazaarsadai/store/views.py
+++ bazaarsadai/store/views.py
@@ -5,12 +5,12 @@ from django.http import JsonResponse
 import json
 import datetime
 
-from django.shortcuts import render,HttpResponse,redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.decorators import login_required
+# from django.shortcuts import render,HttpResponse,redirect
+# from django.contrib.auth.models import User
+# from django.contrib.auth import authenticate,login,logout
+# from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='login')
+# @login_required(login_url='login')
 def store(request):
     if request.user.is_authenticated:
         customer = request.user.customer
@@ -117,38 +117,73 @@ def processOrder(request):
     return JsonResponse('Payment complete!', safe=False)
 
 
-def SignupPage(request):
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import SignUpForm
+
+def signup(request):
     if request.method == 'POST':
-        uname = request.POST.get('username')
-        email = request.POST.get('email')
-        pass1 = request.POST.get('password1')
-        pass2 = request.POST.get('password2')
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('store:store_page')  # Adjust 'store_page' according to your URL pattern
+    else:
+        form = SignUpForm()
+    return render(request, 'store/signup.html', {'form': form})
 
-        if pass1 != pass2:
-            return HttpResponse("Your password and confrom password are not Same!!")
-        else:
-
-            my_user = User.objects.create_user(uname, email, pass1)
-            my_user.save()
-            return redirect('login')
-
-    return render(request, 'signup.html')
-
-
-def LoginPage(request):
+def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
-        pass1 = request.POST.get('pass')
-        user = authenticate(request, username=username, password=pass1)
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('store')
+            return redirect('store:store_page')  # Adjust 'store_page' according to your URL pattern
         else:
-            return HttpResponse("Username or Password is incorrect!!!")
+            return render(request, 'store/login.html', {'error_message': 'Invalid username or password'})
+    else:
+        return render(request, 'store/login.html')
 
-    return render(request, 'login.html')
 
 
-def LogoutPage(request):
-    logout(request)
-    return redirect('login')
+# def SignupPage(request):
+#     if request.method == 'POST':
+#         uname = request.POST.get('username')
+#         email = request.POST.get('email')
+#         pass1 = request.POST.get('password1')
+#         pass2 = request.POST.get('password2')
+#
+#         if pass1 != pass2:
+#             return HttpResponse("Your password and confrom password are not Same!!")
+#         else:
+#
+#             my_user = User.objects.create_user(uname, email, pass1)
+#             my_user.save()
+#             return redirect('login')
+#
+#     return render(request, 'signup.html')
+#
+#
+# def LoginPage(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         pass1 = request.POST.get('pass')
+#         user = authenticate(request, username=username, password=pass1)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('store')
+#         else:
+#             return HttpResponse("Username or Password is incorrect!!!")
+#
+#     return render(request, 'login.html')
+#
+#
+# def LogoutPage(request):
+#     logout(request)
+#     return redirect('login')
+
