28c20dcf8323cdfe2df095e0a0c5b5f948b21125
AfsanaSiddika <afsanaesha584@gmail.com>
2024-04-29 18:39:23 +0600

    updated signup function

--- bazaarsadai/store/views.py
+++ bazaarsadai/store/views.py
@@ -122,19 +122,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import SignUpForm
 
+
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('store:store_page')  # Adjust 'store_page' according to your URL pattern
+            return redirect('store_page')  # Redirect to your store page
     else:
         form = SignUpForm()
-    return render(request, 'store/signup.html', {'form': form})
+    return render(request, 'signup.html', {'form': form})
+
 
 def login_view(request):
     if request.method == 'POST':
